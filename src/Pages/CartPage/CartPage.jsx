1aadbad8fafb0513941bbb295bf4277e6e0d744a
unknown <devakumarmurugan18@gmail.com>
2024-04-26 16:19:11 +0530

    imple

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -12,18 +12,17 @@ const CartPage = () => {
 
   return (
     <div className='c-page'>
-      <h2>Cart</h2>
       {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
+        <p className='e-c-a'>Your cart is empty.</p>
       ) : (
         <div>
           <ul className='p-unset'>
             {cartItems.map(item => (
-              <li className='cart-products row-cols-4 d-flex align-items-center ' key={item.id}><img className='c-p-image ' src={item.image}/><div className='p-cont'><p className='p-details'>{item.pname}</p>
+              <li className='cart-products row-cols-4 d-flex align-items-center justify-content-between align-content-center ' key={item.id}><img className='c-p-image ' src={item.image}/><div  className='p-cont'><p className='p-details'>{item.pname}</p>
               <p className='p-details'>Price: ${item.price}</p>
-              <p className='p-details'>Quantity:{item.count}</p></div>
+              <button className='remove-btn' onClick={()=>dispatch(removeItem({id:item.id}))}>remove</button>
+              </div>
               <div className='btn-cont'><ArrowBackIosNewIcon onClick={()=>dispatch(countLess({id:item.id}))} />{item.count}<ArrowForwardIosIcon onClick={()=>dispatch(countAdd({id:item.id}))} /></div>
-              <div><button className='remove-btn' onClick={()=>dispatch(removeItem({id:item.id}))}>remove</button></div>
               </li>
              
             ))}
