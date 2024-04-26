0c143b1af072e0447b5955684def625000dbf83f
unknown <devakumarmurugan18@gmail.com>
2024-04-26 17:24:38 +0530

    css change

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Header.css';
+import { useSelector } from 'react-redux';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import SearchIcon from '@mui/icons-material/Search';
 import { useDispatch } from 'react-redux';
@@ -8,6 +9,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   const dispatch = useDispatch();
+  const products = useSelector(state => state.cart.cart.items);
   const [searchQuery, setSearchQuery] = useState('');
 
   const handleSearch = (event) => {
@@ -15,6 +17,10 @@ const Header = () => {
     setSearchQuery(query);
     dispatch(filterByName({ query: query.trim() }));
   };
+
+
+  const totalItems = products.reduce((acc, curr) => acc + curr.count, 0);
+  console.log(totalItems);
 
   return (
     <div className='header'>
@@ -31,8 +37,12 @@ const Header = () => {
           <SearchIcon />
         </div>
       </div>
-      <Link to="/cartpage">
-      <ShoppingCartIcon style={{ fontSize: '6vh' ,color:"black" }}/> </Link>
+      <div className='cart-badge'>
+        <div className='badge'>{totalItems}</div>
+        <Link to="/cartpage">
+          <ShoppingCartIcon style={{ fontSize: '6vh', color: 'black' }} />
+        </Link>
+      </div>
     </div>
   );
 };
